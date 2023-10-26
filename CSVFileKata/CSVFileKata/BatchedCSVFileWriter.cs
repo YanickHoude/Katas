9051b84989519c3c00b6aa599deaebf79f0795de
Yanick Houde <yhoude88@gmail.com>
2023-10-25 17:20:48 -0700

    feat: implemented base case of CSVFileKata requirement 2

--- CSVFileKata/CSVFileKata/BatchedCSVFileWriter.cs
+++ CSVFileKata/CSVFileKata/BatchedCSVFileWriter.cs
@@ -12,7 +12,19 @@
         public void Write(string filename, List<Customer> customers)
         {
             var baseFileName = Path.GetFileNameWithoutExtension(filename);
-            _csvFileWriter.Write(baseFileName + "1.csv", customers);
+
+            if (customers.Count > 10)
+            {
+                _csvFileWriter.Write(baseFileName + "1.csv", customers.Take(10).ToList());
+                _csvFileWriter.Write(baseFileName + "2.csv", customers.Skip(10).ToList());
+
+            }
+            else
+            {
+                _csvFileWriter.Write(baseFileName + "1.csv", customers);
+
+            }
+
         }
     }
 }
--- CSVFileKata/CSVFileKataTests/BatchedCSVFileWriterTests.cs
+++ CSVFileKata/CSVFileKataTests/BatchedCSVFileWriterTests.cs
@@ -32,6 +32,89 @@ namespace CSVFileKataTests
                     //assert
                     csvFileWriter.Received(1).Write("customers1.csv", customers);
                 }
+
+                [Test]
+                public void Given6Customers_ShouldWriteCustomersDataAsCSVLineToFilenameWithConcatenated1()
+                {
+                    //arrange
+                    var customers = new List<Customer>
+                    {
+                        GenerateCustomer("a","1"),
+                        GenerateCustomer("b","2"),
+                        GenerateCustomer("c","3"),
+                        GenerateCustomer("d","4"),
+                        GenerateCustomer("e","5"),
+                        GenerateCustomer("f","6"),
+
+                    };
+
+                    var csvFileWriter = Substitute.For<ICustomerCSVFileWriter>();
+                    var fileName = "customers.csv";
+                    var sut = new BatchedCSVFileWriter(csvFileWriter);
+
+                    //act
+                    sut.Write(fileName, customers);
+
+                    //assert
+                    csvFileWriter.Received(1).Write("customers1.csv", customers);
+                }
+
+            }
+
+            public class MultipleFiles
+            {
+                [Test]
+                public void Given12custuomers_ShouldWriteFirst10CustomersToFilenameWithConcatenated1AndWriteRemaining2CustomersToFilenameWithConcatenated2()
+                {
+                    //arrange
+                    var customers = new List<Customer>
+                    {
+                        GenerateCustomer("a","1"),
+                        GenerateCustomer("b","2"),
+                        GenerateCustomer("c","3"),
+                        GenerateCustomer("d","4"),
+                        GenerateCustomer("e","5"),
+                        GenerateCustomer("f","6"),
+                        GenerateCustomer("g","7"),
+                        GenerateCustomer("h","8"),
+                        GenerateCustomer("i","9"),
+                        GenerateCustomer("j","10"),
+                        GenerateCustomer("k","11"),
+                        GenerateCustomer("l","12")
+                    };
+
+                    var csvFileWriter = new FakeCSVFileWriter();
+                    var filename = "customers.csv";
+                    var sut = new BatchedCSVFileWriter(csvFileWriter);
+
+                    //act
+                    sut.Write(filename, customers);
+
+                    //assert
+                    //Assert.AreEqual(2, csvFileWriter.Calls.Count());
+                    Assert.AreEqual("customers1.csv", csvFileWriter.Calls[0].Filename);
+                    CollectionAssert.AreEquivalent(customers.Take(10).ToList(), csvFileWriter.Calls[0].Customers);
+
+                    //Assert.AreEqual(2, csvFileWriter.Calls.Count());
+                    Assert.AreEqual("customers2.csv", csvFileWriter.Calls[1].Filename);
+                    CollectionAssert.AreEquivalent(customers.Skip(10).ToList(), csvFileWriter.Calls[1].Customers);
+
+
+                    //csvFileWriter.Received(1).Write("customers1.csv", customers.Take(10).ToList());
+                    //csvFileWriter.Received(1).Write("customers2.csv", customers.Skip(10).ToList());
+
+                }
+
+                private class FakeCSVFileWriter: ICustomerCSVFileWriter
+                {
+                    public List<(string Filename, List<Customer> Customers)> Calls { get; private set; } = new();
+
+                    public void Write(string filename, List<Customer> customers)
+                    {
+                        Calls.Add((filename, customers));
+                    }
+                }
+
             }
 
 
@@ -93,4 +176,3 @@ namespace CSVFileKataTests
         }
     }
 }
-
